f8af21606dd5143dbf6a77767688af669a839c2d
BhagirathSarvaiya <61153266+BhagirathSarvaiya@users.noreply.github.com>
2021-03-19 21:05:49 +0530

    Create ClosestNumbers.py

---
+++ Solutions/problem solving/ClosestNumbers.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the closestNumbers function below.
+n =  input()
+nums = list(map(int,input().strip().split()))
+nums.sort()
+lowestDiff = nums[1]-nums[0]
+res = [nums[0],nums[1]]
+for i in range(1,len(nums)-1):            
+    if nums[i+1]-nums[i] < lowestDiff:
+        res = []   
+        res.append(nums[i])
+        res.append(nums[i+1])
+        lowestDiff = abs(nums[i+1]-nums[i])
+    elif nums[i+1]-nums[i] == lowestDiff:
+        res.append(nums[i])
+        res.append(nums[i+1])
+
+print(*res)
