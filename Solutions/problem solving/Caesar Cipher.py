2c47bdd160336c43eaa52ad41a949713698d2178
BhagirathSarvaiya <61153266+BhagirathSarvaiya@users.noreply.github.com>
2021-03-22 21:27:49 +0530

    Create Caesar Cipher.py

---
+++ Solutions/problem solving/Caesar Cipher.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the caesarCipher function below.
+def caesarCipher(s, k):
+    for i in range(len(s)):
+        if 97 <= ord(s[i]) <= 122:
+            s = s[:i] + chr(((ord(s[i])-97+k)%26)+97) + s[i+1:]
+        elif 65 <= ord(s[i]) <= 90:
+            s = s[:i] + chr(((ord(s[i])-65+k)%26)+65) + s[i+1:]
+    
+    print(s)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    s = input()
+
+    k = int(input())
+
+    result = caesarCipher(s, k)
